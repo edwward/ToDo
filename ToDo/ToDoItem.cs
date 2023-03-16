b0e3086dd21e8cce8c43b8e3523affdb67997db5
Eduard Feuereis <edwward@seznam.cz>
2023-03-16 11:07:42 +0100

    Add project files.

---
+++ ToDo/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ ToDo/wwwroot/js/interop.js
@@ -0,0 +1,11 @@
+﻿window.getValue = function (id) {
+    return document.getElementById(id).value;
+}
+
+window.setValue = function (id, value) {
+    document.getElementById(id).value = value;
+}
+
+window.focusElement = function (id) {
+    document.getElementById(id).focus();
+}
